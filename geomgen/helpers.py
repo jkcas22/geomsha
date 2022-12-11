cfc7fdd15e248c08bc3c398fa9721b831545ed1e
jkcas22@gmail.com <jkcas22@gmail.com>
2022-12-11 23:06:45 +0100

    stress random forest with modified data

--- geomgen/helpers.py
+++ geomgen/helpers.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 
 
 def gen_grid_image(images, grid):
@@ -30,3 +33,31 @@ def gen_grid_image(images, grid):
         grid_pos.append((x,y))
         grid_img[1+(img_size+1)*x:1+(img_size+1)*x+img_size,1+(img_size+1)*y:1+(img_size+1)*y+img_size] = img
     return (grid_img, grid_pos)
+
+
+def show_grid_image(images, grid, file):
+    """Generates a single image from a list of images arranged in a grid and shows it 
+
+    Parameters
+    ----------
+    images : ndarray
+        List of images
+    grid : tuple
+        The grid size (n,m)
+    file : string
+        path to temporary location of image
+    
+    Returns
+    -------
+    grid_pos : list
+        A list of tuples representing all positions (x,y) in the grid (n,m)
+    """
+
+    grid_image, grid_pos = gen_grid_image(images, grid)
+    
+    fig = plt.figure(figsize=(grid_image.shape[1],grid_image.shape[0]),dpi=1)
+    fig.figimage(grid_image, cmap='gray')
+    fig.savefig(file)
+    plt.close()
+    plt.imshow(mpimg.imread(file))
+    return grid_pos
